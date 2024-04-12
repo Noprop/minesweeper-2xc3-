ef63cf81324649fa40aff48daaa3628fab7fb944
Noprop <12greg21@gmail.com>
2024-04-12 11:19:44 -0400

    add dijkstra

--- final_p1.py
+++ final_p1.py
@@ -47,7 +47,6 @@ class WeightedGraph:
                 G.add_edge(node, neighbor, weight=weight)
 
         # use the spring layout, but increase the distance between the nodes using k
-        # pos = nx.spring_layout(G, k=0.1, iterations=5)
         pos = nx.spring_layout(G, k=5/math.sqrt(self.get_number_of_nodes()), iterations=5)
 
         # create figure, edge labels, and draw
@@ -90,7 +89,19 @@ static_graph.add_edge(4, 2, 5)
 static_graph.add_edge(4, 5, 6)
 static_graph.add_edge(5, 2, -6)
 static_graph.add_edge(5, 6, 4)
-static_graph.display_graph()
+
+static_graph_positive = WeightedGraph(7)
+static_graph_positive.add_edge(1, 0, 6)
+static_graph_positive.add_edge(1, 5, 2)
+static_graph_positive.add_edge(1, 3, 3)
+static_graph_positive.add_edge(1, 6, 4)
+static_graph_positive.add_edge(1, 4, 4)
+static_graph_positive.add_edge(2, 6, 7)
+static_graph_positive.add_edge(4, 0, 2)
+static_graph_positive.add_edge(4, 2, 5)
+static_graph_positive.add_edge(4, 5, 6)
+static_graph_positive.add_edge(5, 2, 6)
+static_graph_positive.add_edge(5, 6, 4)
 
 
 def bellmanFord(g: WeightedGraph, s: int, k: int):
@@ -115,4 +126,167 @@ def bellmanFord(g: WeightedGraph, s: int, k: int):
 
     return (dist, prev)
 
-bellmanFord(static_graph, 1, 1)
+bellmanFord(static_graph, 1, 10)
+
+def dijkstra(Graph,source,destination):
+
+    visited = {}
+    distance = {}
+
+    # create empty queue
+    Q = MinHeap([])
+
+    for i in range(Graph.get_number_of_nodes()):
+
+        visited[i] = False
+        distance[i] = float("inf")
+
+        # insert the nodes in the minheap
+        Q.insert(Item(i, float("inf")))
+
+    print(Q)
+
+    # assign 0 to source 
+    Q.decrease_key(source, 0)
+    distance[source] = 0
+
+    while not (Q.is_empty() or visited[destination]):
+        # get current node
+        current_node = Q.extract_min().value
+        visited[current_node] = True
+
+        for neighbour in Graph.graph[current_node]:
+            # get weight of current node
+            edge_weight = Graph.get_weights(current_node, neighbour)
+
+            temp = distance[current_node] + edge_weight
+
+            # not visited yet
+            if not visited[neighbour]:
+                if temp < distance[neighbour]:
+                    distance[neighbour] = temp
+                    Q.decrease_key(neighbour, temp)
+
+    return distance[destination]
+
+
+class MinHeap:
+    def __init__(self, data):
+        self.items = data
+        self.length = len(data)
+        self.build_heap()
+
+        # add a map based on input node
+        self.map = {}
+        for i in range(self.length):
+            self.map[self.items[i].value] = i
+
+    def find_left_index(self,index):
+        return 2 * (index + 1) - 1
+
+    def find_right_index(self,index):
+        return 2 * (index + 1)
+
+    def find_parent_index(self,index):
+        return (index + 1) // 2 - 1  
+    
+    def sink_down(self, index):
+        smallest_known_index = index
+
+        if self.find_left_index(index) < self.length and self.items[self.find_left_index(index)].key < self.items[index].key:
+            smallest_known_index = self.find_left_index(index)
+
+        if self.find_right_index(index) < self.length and self.items[self.find_right_index(index)].key < self.items[smallest_known_index].key:
+            smallest_known_index = self.find_right_index(index)
+
+        if smallest_known_index != index:
+            self.items[index], self.items[smallest_known_index] = self.items[smallest_known_index], self.items[index]
+            
+            # update map
+            self.map[self.items[index].value] = index
+            self.map[self.items[smallest_known_index].value] = smallest_known_index
+
+            # recursive call
+            self.sink_down(smallest_known_index)
+
+    def build_heap(self,):
+        for i in range(self.length // 2 - 1, -1, -1):
+            self.sink_down(i) 
+
+    def insert(self, node):
+        if len(self.items) == self.length:
+            self.items.append(node)
+        else:
+            self.items[self.length] = node
+        self.map[node.value] = self.length
+        self.length += 1
+        self.swim_up(self.length - 1)
+
+    def insert_nodes(self, node_list):
+        for node in node_list:
+            self.insert(node)
+
+    def swim_up(self, index):
+        
+        while index > 0 and self.items[index].key < self.items[self.find_parent_index(index)].key:
+            #swap values
+            self.items[index], self.items[self.find_parent_index(index)] = self.items[self.find_parent_index(index)], self.items[index]
+            #update map
+            self.map[self.items[index].value] = index
+            self.map[self.items[self.find_parent_index(index)].value] = self.find_parent_index(index)
+            index = self.find_parent_index(index)
+
+    def get_min(self):
+        if len(self.items) > 0:
+            return self.items[0]
+
+    def extract_min(self,):
+        #xchange
+        self.items[0], self.items[self.length - 1] = self.items[self.length - 1], self.items[0]
+        #update map
+        self.map[self.items[self.length - 1].value] = self.length - 1
+        self.map[self.items[0].value] = 0
+
+        min_node = self.items[self.length - 1]
+        self.length -= 1
+        self.map.pop(min_node.value)
+        self.sink_down(0)
+        return min_node
+
+    def decrease_key(self, value, new_key):
+        if new_key >= self.items[self.map[value]].key:
+            return
+        index = self.map[value]
+        self.items[index].key = new_key
+        self.swim_up(index)
+
+    def get_element_from_value(self, value):
+        return self.items[self.map[value]]
+
+    def is_empty(self):
+        return self.length == 0
+    
+    def __str__(self):
+        height = math.ceil(math.log(self.length + 1, 2))
+        whitespace = 2 ** height + height
+        s = ""
+        for i in range(height):
+            for j in range(2 ** i - 1, min(2 ** (i + 1) - 1, self.length)):
+                s += " " * whitespace
+                s += str(self.items[j]) + " "
+            s += "\n"
+            whitespace = whitespace // 2
+        return s
+    
+    def contains(self, value):
+        return value in self.map
+
+class Item:
+    def __init__(self, value, key):
+        self.key = key
+        self.value = value
+    
+    def __str__(self):
+        return "(" + str(self.key) + "," + str(self.value) + ")"
+
+
